801f98c911a40c0563797c9706a710a59a051104
wfchandler <10663805+wfchandler@users.noreply.github.com>
2025-10-02 02:58:51 +0000

    dist 1.0.9

--- Formula/dist.rb
+++ Formula/dist.rb
@@ -1,25 +1,25 @@
 class Dist < Formula
   desc "Shippable application packaging for Rust"
   homepage "https://opensource.axo.dev/cargo-dist/"
-  version "1.0.8"
+  version "1.0.9"
   if OS.mac?
     if Hardware::CPU.arm?
-      url "https://github.com/oxidecomputer/cargo-dist/releases/download/v1.0.8/dist-aarch64-apple-darwin.tar.xz"
-      sha256 "d6574279269bec65588bdcc28ef26978621781438c313e9fa570beab32462c4c"
+      url "https://github.com/oxidecomputer/cargo-dist/releases/download/v1.0.9/dist-aarch64-apple-darwin.tar.xz"
+      sha256 "c5284d13baa7248511295e1b0576f5e36f4e9abe31b6c9315b895d979ba81680"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/oxidecomputer/cargo-dist/releases/download/v1.0.8/dist-x86_64-apple-darwin.tar.xz"
-      sha256 "1372c4fc22af32de130e2a235407882dca1baf17b31eb1c61af5d2824f2b4188"
+      url "https://github.com/oxidecomputer/cargo-dist/releases/download/v1.0.9/dist-x86_64-apple-darwin.tar.xz"
+      sha256 "bc502378be9f28f18398d10092bae8d436fb8a64f8ea6c66df54b778069ddbd4"
     end
   end
   if OS.linux?
     if Hardware::CPU.arm?
-      url "https://github.com/oxidecomputer/cargo-dist/releases/download/v1.0.8/dist-aarch64-unknown-linux-gnu.tar.xz"
-      sha256 "5bf955a5f63054a446aa175b5b6c2b045ac065702f88477de796d21147b5abd4"
+      url "https://github.com/oxidecomputer/cargo-dist/releases/download/v1.0.9/dist-aarch64-unknown-linux-gnu.tar.xz"
+      sha256 "f94e43408a765c4cdfb08d602da44111c7a7777674dd533d66cc7b8a7827f201"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/oxidecomputer/cargo-dist/releases/download/v1.0.8/dist-x86_64-unknown-linux-gnu.tar.xz"
-      sha256 "0e6adcfc3968a5aab98760518431c8de27651047bdb24722f57347e1a8ef19b0"
+      url "https://github.com/oxidecomputer/cargo-dist/releases/download/v1.0.9/dist-x86_64-unknown-linux-gnu.tar.xz"
+      sha256 "a43672eade42d0a31def1c6c010b46e7afaa84a3d3881b4130347ee77f7532b2"
     end
   end
   license any_of: ["MIT", "Apache-2.0"]
