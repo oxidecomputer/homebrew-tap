8f35d98836d1e9fe4602ed8ec847f3af9c3365f3
wfchandler <10663805+wfchandler@users.noreply.github.com>
2025-10-08 16:40:49 +0000

    dist@1.0 1.0.14

---
+++ Formula/dist@1.0.rb
@@ -0,0 +1,106 @@
+class DistAT10 < Formula
+  desc "Shippable application packaging for Rust"
+  homepage "https://opensource.axo.dev/cargo-dist/"
+  version "1.0.14"
+  if OS.mac?
+    if Hardware::CPU.arm?
+      url "https://github.com/oxidecomputer/cargo-dist/releases/download/v1.0.14/dist-aarch64-apple-darwin.tar.xz"
+      sha256 "a441891f953f4d323b86e9e29123fce3b5d524e4e1e7c717c5c47b358fcb2d78"
+    end
+    if Hardware::CPU.intel?
+      url "https://github.com/oxidecomputer/cargo-dist/releases/download/v1.0.14/dist-x86_64-apple-darwin.tar.xz"
+      sha256 "186656be1df4f194f7c6878b0ced10060ba81bca91e90077e7d935790a73e3d9"
+    end
+  end
+  if OS.linux?
+    if Hardware::CPU.arm?
+      url "https://github.com/oxidecomputer/cargo-dist/releases/download/v1.0.14/dist-aarch64-unknown-linux-gnu.tar.xz"
+      sha256 "653fe1bcab4c0ac989c014ff847f46a2b0a04a45af477f1e825f3ece94e51bb1"
+    end
+    if Hardware::CPU.intel?
+      url "https://github.com/oxidecomputer/cargo-dist/releases/download/v1.0.14/dist-x86_64-unknown-linux-gnu.tar.xz"
+      sha256 "09b522bc5caa6763cce5a1ccbd9233db3f5e627e40dcff52577f1aaac67266b4"
+    end
+  end
+  license any_of: ["MIT", "Apache-2.0"]
+
+  BINARY_ALIASES = {
+    "aarch64-apple-darwin":               {
+      dist: [
+        "cargo-dist",
+      ],
+    },
+    "aarch64-unknown-linux-gnu":          {
+      dist: [
+        "cargo-dist",
+      ],
+    },
+    "aarch64-unknown-linux-musl-dynamic": {
+      dist: [
+        "cargo-dist",
+      ],
+    },
+    "aarch64-unknown-linux-musl-static":  {
+      dist: [
+        "cargo-dist",
+      ],
+    },
+    "x86_64-apple-darwin":                {
+      dist: [
+        "cargo-dist",
+      ],
+    },
+    "x86_64-pc-windows-gnu":              {
+      "dist.exe": [
+        "cargo-dist.exe",
+      ],
+    },
+    "x86_64-unknown-linux-gnu":           {
+      dist: [
+        "cargo-dist",
+      ],
+    },
+    "x86_64-unknown-linux-musl-dynamic":  {
+      dist: [
+        "cargo-dist",
+      ],
+    },
+    "x86_64-unknown-linux-musl-static":   {
+      dist: [
+        "cargo-dist",
+      ],
+    },
+  }.freeze
+
+  def target_triple
+    cpu = Hardware::CPU.arm? ? "aarch64" : "x86_64"
+    os = OS.mac? ? "apple-darwin" : "unknown-linux-gnu"
+
+    "#{cpu}-#{os}"
+  end
+
+  def install_binary_aliases!
+    BINARY_ALIASES[target_triple.to_sym].each do |source, dests|
+      dests.each do |dest|
+        bin.install_symlink bin/source.to_s => dest
+      end
+    end
+  end
+
+  def install
+    bin.install "dist" if OS.mac? && Hardware::CPU.arm?
+    bin.install "dist" if OS.mac? && Hardware::CPU.intel?
+    bin.install "dist" if OS.linux? && Hardware::CPU.arm?
+    bin.install "dist" if OS.linux? && Hardware::CPU.intel?
+
+    install_binary_aliases!
+
+    # Homebrew will automatically install these, so we don't need to do that
+    doc_files = Dir["README.*", "readme.*", "LICENSE", "LICENSE.*", "CHANGELOG.*"]
+    leftover_contents = Dir["*"] - doc_files
+
+    # Install any leftover files in pkgshare; these are probably config or
+    # sample files.
+    pkgshare.install(*leftover_contents) unless leftover_contents.empty?
+  end
+end
